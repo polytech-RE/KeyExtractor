a27f9c5c69f73b341845cda2a4d47c0163dc5e15
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-04-22 14:32:22 +0200

    creation appli swift

---
+++ KeyExtractor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyExtractor
+//
+//  Created by remy on 22/04/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeyExtractor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeyExtractor
+//
+//  Created by remy on 22/04/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
