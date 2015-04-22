541c08c60e3d954d8bddf3cdaf015e90938e11a6
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-04-22 14:12:21 +0200

    création projet

---
+++ KeyExtractor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KeyExtractor
+//
+//  Created by remy on 22/04/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ KeyExtractor/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  KeyExtractor
+//
+//  Created by remy on 22/04/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
