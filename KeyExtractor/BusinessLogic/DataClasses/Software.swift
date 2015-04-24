6231656881c602555d980e8186c9df46c1ee2fbc
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-04-24 14:52:30 +0200

    add type and fixed error to class software

--- KeyExtractor/BusinessLogic/DataClasses/Software.swift
+++ KeyExtractor/BusinessLogic/DataClasses/Software.swift
@@ -7,39 +7,35 @@
 //
 
 import Foundation
-
+/*
 class Software {
     
-    private var name
-    private var vendor
-    private var version
-    private var computerName
-    private var scanTime
+    private var name : String
+    private var vendor : String
+    private var version : String
 
 /**
 * The licence key (ex 'AAA-BBBB-CCCCC-EEEEE')
 */
-    private key
+  //  private key : String
 
 /**
 * Registry path of the licence key
 */
-    private keyRegistryPath
+  //  private keyRegistryPath
 
 /**
 * Registry value which contains the licence key (ex 'ProductID')
 */
-    private keyRegistryValue
-
-public Software(String name, String vendor, String version, String computerName, String scanTime, String userName, String key, String keyRegistryPath, String keyRegistryValue) {
+   // private keyRegistryValue
+/*
+ init(String name, String vendor, String version, String computerName, String scanTime, String userName, String key, String keyRegistryPath, String keyRegistryValue) {
     self.name = name;
     self.vendor = vendor;
     self.version = version;
-    this.computerName = computerName;
-    this.scanTime = scanTime;
     this.userName = userName;
     this.key = key;
     this.keyRegistryPath = keyRegistryPath;
     this.keyRegistryValue = keyRegistryValue;
-}
-}
+} */
+}*/
