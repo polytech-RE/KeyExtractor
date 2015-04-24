8dcda79ef6ae61e38002882654a2c2d5b1645c99
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-04-24 08:53:17 +0200

    move File and implementation of File

---
+++ KeyExtractor/BusinessLogic/DataClasses/File.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  KeyExtractor
+//
+//  Created by polytech on 23/04/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+import Foundation
+
+class File{
+    
+    var path: String
+    
+    //initializers
+    
+    //initialize the Abstract object File with the String path
+    init(path: String){
+        self.path = path
+    }
+
+}
--- KeyExtractor/BusinessLogic/DataClasses/FilePlist.swift
+++ KeyExtractor/BusinessLogic/DataClasses/FilePlist.swift
@@ -1,8 +1,8 @@
 //
-//  File.swift
+//  FilePlist.swift
 //  KeyExtractor
 //
-//  Created by polytech on 23/04/2015.
+//  Created by remy on 24/04/2015.
 //  Copyright (c) 2015 polytech-RE. All rights reserved.
 //
 
