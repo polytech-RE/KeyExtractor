ebfc9b8e453a6d5677709df32990b2cd10e6babf
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-04-27 10:09:31 +0200

    class software

--- KeyExtractor/BusinessLogic/DataClasses/Software.swift
+++ KeyExtractor/BusinessLogic/DataClasses/Software.swift
@@ -7,35 +7,37 @@
 //
 
 import Foundation
-/*
+
 class Software {
     
     private var name : String
     private var vendor : String
     private var version : String
-
-/**
-* The licence key (ex 'AAA-BBBB-CCCCC-EEEEE')
-*/
-  //  private key : String
-
-/**
-* Registry path of the licence key
-*/
-  //  private keyRegistryPath
-
-/**
-* Registry value which contains the licence key (ex 'ProductID')
-*/
-   // private keyRegistryValue
-/*
- init(String name, String vendor, String version, String computerName, String scanTime, String userName, String key, String keyRegistryPath, String keyRegistryValue) {
-    self.name = name;
-    self.vendor = vendor;
-    self.version = version;
-    this.userName = userName;
-    this.key = key;
-    this.keyRegistryPath = keyRegistryPath;
-    this.keyRegistryValue = keyRegistryValue;
-} */
-}*/
+    
+    /*
+    The licence key (ex 'AAA-BBBB-CCCCC-EEEEE')
+    */
+    private var key : String
+    
+    /*
+    Registry path of the licence key
+    */
+    private var keyRegistryPath: String
+    
+    /*
+    Registry value which contains the licence key (ex 'ProductID')
+    */
+    private var keyRegistryValue: String
+    
+    private var userName: String
+    
+    init(name: String, vendor: String, version: String, computerName: String, userName: String, key: String, keyRegistryPath: String, keyRegistryValue: String) {
+        self.name = name
+        self.vendor = vendor
+        self.version = version
+        self.userName = userName
+        self.key = key
+        self.keyRegistryPath = keyRegistryPath
+        self.keyRegistryValue = keyRegistryValue
+    }
+}
