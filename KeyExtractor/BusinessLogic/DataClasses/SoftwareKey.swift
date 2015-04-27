149dded0568e00f6693427c9219567de96609ad3
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-04-27 09:59:17 +0200

    add class softwareKey

--- KeyExtractor/BusinessLogic/DataClasses/SoftwareKey.swift
+++ KeyExtractor/BusinessLogic/DataClasses/SoftwareKey.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+/*  
+    Classe representant les cles licences ponderees,
+    avec une valeur et un poids.
+    Ce poids est affecte en fonction de la forme de 
+    la cle.
+*/
+
+class SoftwareKey{
+    
+    /*  
+        The key Value
+        (for example: AAAA-AAAA-AAAA-AAAA)
+    */
+    private let key: String
+    
+    /*
+        The key weight,
+        the more weight is great
+        the more chance that it is a real key.
+    */
+    private let weight: Int
+    
+    init (key: String, weight: Int){
+        self.key = key
+        self.weight = weight
+    }
+}
