a01349a7ece839d81b000cfae7764092d039f38f
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-05-05 15:09:54 +0200

    controller view

---
+++ KeyExtractor/UserInterface/SoftwareViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SoftwareViewController.swift
+//  KeyExtractor
+//
+//  Created by remy on 05/05/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+import Cocoa
+
+class SoftwareViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
