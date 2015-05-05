7702a146f36197b6436ec2c1f5ce453bbe62247a
elie <elie.gallet@gmail.com>
2015-05-05 08:10:38 +0200

    view (modify the txt)

--- KeyExtractor/UserInterface/ViewController.swift
+++ KeyExtractor/UserInterface/ViewController.swift
@@ -10,6 +10,11 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var name: NSTextField!
+    @IBOutlet weak var licensepath: NSTextField!
+    @IBOutlet weak var format: NSPopUpButton!
+    @IBOutlet weak var infopath: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,5 +28,10 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func send(sender: AnyObject) {
+        // recuperer champ
+        // enrengister fichier
+        
+    }
 }
 
