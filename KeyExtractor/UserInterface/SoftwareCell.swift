f11c8206527806feb6d9f4cde96240dcdab48a73
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-05-06 12:19:08 +0200

    softwareCell

---
+++ KeyExtractor/UserInterface/SoftwareCell.swift
@@ -0,0 +1,19 @@
+//
+//  SoftwareCell.swift
+//  KeyExtractor
+//
+//  Created by remy on 06/05/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+import Cocoa
+
+class SoftwareCell: NSTableCellView {
+    
+
+    @IBOutlet weak var softwareName: NSTextField!
+
+    func loadItem(#name: String) {
+        softwareName.stringValue = name
+    }
+}
