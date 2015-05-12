bd67c04aa5eab4fadab2e5907efca35c7f877cdc
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-05-12 11:51:35 +0200

    softwareCheckboxCell

---
+++ KeyExtractor/UserInterface/SoftwareCheckBoxCell.swift
@@ -0,0 +1,15 @@
+//
+//  SoftwareCheckBox.swift
+//  KeyExtractor
+//
+//  Created by remy on 12/05/2015.
+//  Copyright (c) 2015 polytech-RE. All rights reserved.
+//
+
+import Cocoa
+
+class SoftwareCheckBoxCell: NSTableCellView {
+    
+  
+   
+}
