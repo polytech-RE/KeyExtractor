359b403803107ff550f3b231dfc8518f1a791344
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-05-26 12:01:57 +0200

    comments for FileFactory

--- KeyExtractor/BusinessLogic/TechnicalClasses/FileFactory.swift
+++ KeyExtractor/BusinessLogic/TechnicalClasses/FileFactory.swift
@@ -8,7 +8,26 @@
 
 import Foundation
 
+///In class-based programming, the factory method pattern is a creational pattern which uses factory methods to deal with the problem of creating objects without specifying the exact class of object that will be created. We use this pattern to create a file. For each format, we create a file.
+
 class FileFactory {
+
+    /**
+    This method allows to create a File.
+    
+    This function checks if the path exists 
+    and create the appropriated file with the format
+    
+    :param: path The path of the file
+    
+    :param: format The format of the file:
+    
+        - xml
+        - plist
+        - txt
+    
+    :returns: a File
+    */
 
     static func createFile(path: String, format: String) -> File?{
         
