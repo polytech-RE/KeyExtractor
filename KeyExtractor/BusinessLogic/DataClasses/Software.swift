566b42cdde4b18e0ade309e48c570f30107e5587
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-05-26 11:36:09 +0200

    comments

--- KeyExtractor/BusinessLogic/DataClasses/Software.swift
+++ KeyExtractor/BusinessLogic/DataClasses/Software.swift
@@ -6,6 +6,10 @@
 //  Copyright (c) 2015 polytech-RE. All rights reserved.
 //
 
+/*!
+@header Software.swift
+*/
+
 import Foundation
 
 public class Software {
@@ -43,53 +47,70 @@ public class Software {
         self.version = version
         self.key = key
         self.sell = 0
-        //self.keys = Set()
     }
     
     //Getters
     
-    /*
-    getter for name
+    /**
+    This method get the name of a software.
+    
+    :returns: the software name
     */
     public func getName() -> String{
         
         return self.name
     }
+
+    /**
+    This method get the copyright of a software.
     
-    /*
-    getter for copyright
+    :returns: the software copyright
     */
     public func getCopyright() -> String{
         
         return self.copyright
     }
    
-    /*
-    getter for version
+    /**
+    This method get the version of a software.
+    
+    :returns: the software version
     */
     public func getVersion() -> String{
         
         return self.version
     }
     
-    /*
-    getter for key
+    /**
+    This method get the key of a software.
+    
+    :returns: the software key
     */
     public func getKey() -> String{
         
         return self.key
     }
     
-    /*
-    getter for the sell value
+    /**
+    This method get the state of a software.
+    
+        - 1 : The user would like to sell the software
+        - 0 : The user would like to keep the software
+    
+    :returns: the software state
     */
     public func getSell() -> Int{
         
         return self.sell
     }
     
-    /*
-    getter for the sell value
+    /**
+    This method allows to set the state of a software.
+    
+    - 1 : The user would like to sell the software
+    - 0 : The user would like to keep the software
+    
+    :params: the software state (0 or 1)
     */
     public func setSell(val: Int) {
         
